48807a817ea4ace80a8ea45795e0013559e5cd73
workIDE <zdan@bk.ru>
2016-04-22 12:16:29 +0300

    library for elements in buil.gradle + ActionBar and Toolbar added in MainActivity.java & activity_main.xml

--- app/src/main/java/com/limitrof/doit/MainActivity.java
+++ app/src/main/java/com/limitrof/doit/MainActivity.java
@@ -1,13 +1,25 @@
 package com.limitrof.doit;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
+    //private ActionBar actionBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setActionBar();
         setContentView(R.layout.activity_main);
+    }
+    private void setActionBar(){
+        toolbar=(Toolbar)findViewById(R.id.toolbar);//переводим во вью приведенное к тулбару из саппорт-библиотеки
+        setSupportActionBar(toolbar);
+        ActionBar actionBar=getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
